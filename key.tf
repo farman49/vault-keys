ff9b565e9e4e7f852ce3e98edb9d836d87ec9e65
Farman Ali <farmanali494917@gmail.com>
2023-06-20 17:57:59 +0500

    Update key.tf
    80 days older keys

--- key.tf
+++ key.tf
@@ -1,35 +1,46 @@
 provider "aws" {
-  region = "us-east-1"  # Update with your desired AWS region
+  region = "us-east-1"  # Replace with your desired AWS region
 }
 
 # provider "vault" {
-#   address = "http://vault.example.com"  # Replace with the address of your Vault server
-#   token   = "your_vault_token"  # Replace with your Vault token
+#   address = "http://vault.example.com"  # Replace with your Vault address
+#   token   = "your-vault-token"  # Replace with your Vault token
 # }
 
-resource "aws_iam_access_key" "test" {
-  user = "admin"  # Replace with the name of your existing IAM user
+data "aws_iam_user" "rohan" {
+  user_name = "rohan"
+}
 
-  lifecycle {
-    create_before_destroy = true  # Ensures the old access key is deleted only after the new one is created
+data "aws_iam_user" "admin" {
+  user_name = "admin"
+}
+
+locals {
+  existing_users = ["rohan", "admin"]
+}
+
+resource "null_resource" "rotate_access_key" {
+  for_each = toset(local.existing_users)
+
+  provisioner "local-exec" {
+    command = <<EOF
+      access_key_metadata=$(aws iam list-access-keys --user-name ${each.key} --query "AccessKeyMetadata[1]")
+      
+      if [ "$access_key_metadata" != "null" ]; then
+        access_key_age=$(($(date +%s) - $(date --date=$(echo $access_key_metadata | jq -r '.CreateDate') +%s)))
+        if (( $access_key_age / 86400 > 80 )); then
+          aws iam delete-access-key --access-key $(echo $access_key_metadata | jq -r '.AccessKeyId') --user-name ${each.key}
+          aws iam create-access-key --user-name ${each.key}
+          
+          # vault write secret/aws_access_keys/${each.key} \
+          #   access_key=$($(aws iam list-access-keys --user-name ${each.key} --query "AccessKeyMetadata[0].AccessKeyId" --output text) 2>/dev/null) \
+          #   secret_key=$($(aws iam list-access-keys --user-name ${each.key} --query "AccessKeyMetadata[0].SecretAccessKey" --output text) 2>/dev/null)
+        else
+          echo "No action required for user ${each.key}."
+        fi
+      else
+        echo "No existing second access key found for user ${each.key}."
+      fi
+    EOF
   }
-
-  # provisioner "local-exec" {
-  #   command = <<EOF
-  #     vault kv put secret/my/path/access_credentials access_key=${aws_iam_access_key.test.id} secret_key=${aws_iam_access_key.test.secret} 
-  #   EOF
-  # }  
 }
-
-output "access_key" {
-  value = aws_iam_access_key.test.id
-}
-
-output "secret_key" {
-  value     = aws_iam_access_key.test.secret
-  sensitive = true
-}
-
-#output "sensitive_secret_key" {
-  #value = aws_iam_access_key.test.secret
-#}
